95c8bb8a2b08d49ef8f6499c17c33bf9b388a6e9
Water_s0urce <76213286+Watersrc-stack@users.noreply.github.com>
2023-06-06 21:45:54 +0200

    Ajout des fonctions d'affichage / sélection du canal radio.

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
+def Display_number(num: number):
+    if num == 1:
+        pass
+    elif num == 2:
+        pass
+    elif num == 3:
+        pass
+    elif num == 4:
+        pass
+    elif num == 5:
+        pass
+    elif num == 6:
+        pass
+    elif num == 7:
+        pass
+    elif num == 8:
+        pass
+    elif num == 9:
+        pass
+
+ncanal = 0
 canal_choiced = 0
 while canal_choiced == 0:
-    if input.button_is_pressed(Button.A):
-        pass
-    elif input.button_is_pressed(Button.B):
-        pass
+    if input.button_is_pressed(Button.A) and ncanal >= 2:
+        ncanal += -1
+        Display_number(ncanal)
+    elif input.button_is_pressed(Button.B) and ncanal <= 8:
+        ncanal += 1
+        Display_number(ncanal)
     elif input.button_is_pressed(Button.AB):
-        pass
-
-
-def on_forever():
-    pass
-basic.forever(on_forever)
+        canal_choiced = 1
+radio.set_group(ncanal)
--- main.ts
+++ main.ts
@@ -1,15 +1,98 @@
-let ncanal = 0
+function Display_number (num: number) {
+    if (num == 1) {
+        images.createImage(`
+            . . # . .
+            . # # . .
+            # . # . .
+            . . # . .
+            . . # . .
+            `).showImage(0)
+    } else if (num == 2) {
+        images.createImage(`
+            . # # # .
+            # . . . #
+            . . . # .
+            . . # . .
+            . # # # #
+            `).showImage(0)
+    } else if (num == 3) {
+        images.createImage(`
+            . # # . .
+            . . . # .
+            . . # . .
+            . . . # .
+            . # # . .
+            `).showImage(0)
+    } else if (num == 4) {
+        images.createImage(`
+            . . # . .
+            . # . . .
+            # . . # .
+            . # # # #
+            . . . # .
+            `).showImage(0)
+    } else if (num == 5) {
+        images.createImage(`
+            . # # # .
+            . # . . .
+            . . # . .
+            . . . # .
+            . # # . .
+            `).showImage(0)
+    } else if (num == 6) {
+        images.createImage(`
+            . . # # .
+            . # . . .
+            . . # # .
+            . # . . #
+            . . # # .
+            `).showImage(0)
+    } else if (num == 7) {
+        images.createImage(`
+            . # # # .
+            . . . # .
+            . . # . .
+            . . # . .
+            . # . . .
+            `).showImage(0)
+    } else if (num == 8) {
+        images.createImage(`
+            . . # # .
+            . # . . #
+            . . # # .
+            . # . . #
+            . . # # .
+            `).showImage(0)
+    } else if (num == 9) {
+        images.createImage(`
+            . . # # .
+            . # . . #
+            . . # # #
+            . . . # .
+            . . # . .
+            `).showImage(0)
+    }
+}
+function clear_screen () {
+    images.createImage(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `).showImage(0)
+}
 let canal_choiced = 0
+let ncanal = 3
 while (canal_choiced == 0) {
     if (input.buttonIsPressed(Button.A) && ncanal >= 2) {
         ncanal += -1
+        Display_number(ncanal)
     } else if (input.buttonIsPressed(Button.B) && ncanal <= 8) {
         ncanal += 1
+        Display_number(ncanal)
     } else if (input.buttonIsPressed(Button.AB)) {
         canal_choiced = 1
     }
 }
 radio.setGroup(ncanal)
-basic.forever(function () {
-	
-})
