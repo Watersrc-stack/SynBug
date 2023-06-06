cb6a4404d9d4a841b47a500386b1f8447ab2eed7
Water_s0urce <76213286+Watersrc-stack@users.noreply.github.com>
2023-06-06 22:05:36 +0200

    Ajout de petites transitions sympathiques

--- main.py
+++ main.py
@@ -1,32 +1,105 @@
 def Display_number(num: number):
     if num == 1:
-        pass
+        images.create_image("""
+            . . # . .
+                        . # # . .
+                        # . # . .
+                        . . # . .
+                        . . # . .
+        """).show_image(0)
     elif num == 2:
-        pass
+        images.create_image("""
+            . # # # .
+                        # . . . #
+                        . . . # .
+                        . . # . .
+                        . # # # #
+        """).show_image(0)
     elif num == 3:
-        pass
+        images.create_image("""
+            . # # . .
+                        . . . # .
+                        . . # . .
+                        . . . # .
+                        . # # . .
+        """).show_image(0)
     elif num == 4:
-        pass
+        images.create_image("""
+            . . # . .
+                        . # . . .
+                        # . . # .
+                        . # # # #
+                        . . . # .
+        """).show_image(0)
     elif num == 5:
-        pass
+        images.create_image("""
+            . # # # .
+                        . # . . .
+                        . . # . .
+                        . . . # .
+                        . # # . .
+        """).show_image(0)
     elif num == 6:
-        pass
+        images.create_image("""
+            . . # # .
+                        . # . . .
+                        . . # # .
+                        . # . . #
+                        . . # # .
+        """).show_image(0)
     elif num == 7:
-        pass
+        images.create_image("""
+            . # # # .
+                        . . . # .
+                        . . # . .
+                        . . # . .
+                        . # . . .
+        """).show_image(0)
     elif num == 8:
-        pass
+        images.create_image("""
+            . . # # .
+                        . # . . #
+                        . . # # .
+                        . # . . #
+                        . . # # .
+        """).show_image(0)
     elif num == 9:
-        pass
-
-ncanal = 0
+        images.create_image("""
+            . . # # .
+                        . # . . #
+                        . . # # #
+                        . . . # .
+                        . . # . .
+        """).show_image(0)
+def clear_screen():
+    images.create_image("""
+        . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+    """).show_image(0)
 canal_choiced = 0
+images.create_image("""
+    . . # # #
+        . . . . .
+        . # # # .
+        . . . . .
+        # # # . .
+""").show_image(0)
+control.wait_micros(3000000)
+clear_screen()
+ncanal = 3
+Display_number(ncanal)
 while canal_choiced == 0:
     if input.button_is_pressed(Button.A) and ncanal >= 2:
-        ncanal += -1
+        ncanal += 0 - 1
         Display_number(ncanal)
     elif input.button_is_pressed(Button.B) and ncanal <= 8:
         ncanal += 1
         Display_number(ncanal)
     elif input.button_is_pressed(Button.AB):
         canal_choiced = 1
+    else:
+        pass
 radio.set_group(ncanal)
--- main.ts
+++ main.ts
@@ -83,16 +83,34 @@ function clear_screen () {
         `).showImage(0)
 }
 let canal_choiced = 0
+music.playSoundEffect(music.createSoundEffect(WaveShape.Noise, 54, 54, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.InBackground)
+images.createImage(`
+    . . # # #
+    . . . . .
+    . # # # .
+    . . . . .
+    # # # . .
+    `).showImage(0)
+control.waitMicros(3000000)
+clear_screen()
 let ncanal = 3
+Display_number(ncanal)
 while (canal_choiced == 0) {
     if (input.buttonIsPressed(Button.A) && ncanal >= 2) {
-        ncanal += -1
+        ncanal += 0 - 1
         Display_number(ncanal)
     } else if (input.buttonIsPressed(Button.B) && ncanal <= 8) {
         ncanal += 1
         Display_number(ncanal)
     } else if (input.buttonIsPressed(Button.AB)) {
         canal_choiced = 1
+        Display_number(ncanal)
+    } else {
+    	
     }
 }
 radio.setGroup(ncanal)
+music.playSoundEffect(music.builtinSoundEffect(soundExpression.giggle), SoundExpressionPlayMode.InBackground)
+images.iconImage(IconNames.Yes).showImage(0)
+control.waitMicros(2000000)
+images.iconImage(IconNames.House).showImage(0)
